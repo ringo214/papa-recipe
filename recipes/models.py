f3c0edd940c5dbd29282219c040c549b3620a753
Rie <r.h.love12250214@gmail.com>
2026-02-28 16:01:10 +0900

    フィルター機能の追加

--- recipes/models.py
+++ recipes/models.py
@@ -3,11 +3,10 @@ from django.db import models
 class Recipe(models.Model):
     title = models.CharField('料理名', max_length=100)
     tool = models.CharField('調理器具（レンジ・炊飯器など）', max_length=50)
-    # ↓ この1行を追加！
+    poster = models.CharField('投稿者', max_length=50, default='りえ')
     image = models.ImageField('完成写真', upload_to='recipes/', null=True, blank=True)
     ingredients = models.TextField('材料')
     instructions = models.TextField('作り方（3ステップ！）')
     created_at = models.DateTimeField('作成日', auto_now_add=True)
-
-    def __str__(self):
+    def str(self):
         return self.title
--- recipes/views.py
+++ recipes/views.py
@@ -2,17 +2,7 @@ from django.shortcuts import render
 from .models import Recipe
 
 def recipe_list(request):
-    # ① 検索バーに入力された文字（q）を受け取る
+    # ① 画面から送られてきた検索・絞り込みの条件を受け取る
     query = request.GET.get('q')
-
-    # ② もし検索キーワードがあったら…
-    if query:
-        # タイトルにキーワードが含まれるレシピだけを探す！
-        recipes = Recipe.objects.filter(title__icontains=query).order_by('-created_at')
-    # ③ キーワードがなかったら（最初の画面）…
-    else:
-        # 今まで通り全部のレシピを新しい順に出す！
-        recipes = Recipe.objects.all().order_by('-created_at')
-
-    # 結果をHTMLに渡す
-    return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
+    tool_query = request.GET.get('tool')
+    poster_query = request.GET.get('poster')
