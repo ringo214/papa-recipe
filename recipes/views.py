4ac7ea6eac9e682a898072d756cf5a78de2d38e7
Rie <r.h.love12250214@gmail.com>
2026-02-28 16:20:29 +0900

    フィルター修正

--- recipes/views.py
+++ recipes/views.py
@@ -6,3 +6,14 @@ def recipe_list(request):
     query = request.GET.get('q')
     tool_query = request.GET.get('tool')
     poster_query = request.GET.get('poster')
+    recipes = Recipe.objects.all().order_by('-created_at')
+    if query:
+        recipes = recipes.filter(title__icontains=query)
+    if tool_query:
+        recipes = recipes.filter(tool=tool_query)
+    if poster_query:
+        recipes = recipes.filter(poster=poster_query)
+    tools = Recipe.objects.values_list('tool', flat=True).distinct()
+    posters = Recipe.objects.values_list('poster', flat=True).distinct()
+    context = {'recipes': recipes, 'tools': tools, 'posters': posters}
+    return render(request, 'recipes/recipe_list.html', context)
